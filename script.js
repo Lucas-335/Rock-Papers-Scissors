79387e48c3c55582c8e826974653bd98d3c9b274
Lucas Aguiar <lucas.335@live.com>
2024-07-02 18:17:49 -0300

    refatoração de codigo finalizada

--- script.js
+++ script.js
@@ -10,89 +10,149 @@ function getComputerChoice(){
         return computer
     }
     else{
-        computer = 'scissors'
+        computer = 'scissor'
         return computer
     }
 }
 
-function getHumanChoice(){
-    let human = parseInt(prompt('Escolha entre: \n[1] pedra | [2] papel | [3] tesoura: '));
-    if (human == 1){
-        return 'rock'
-    }
-    else if (human == 2){
-        return 'paper'
-    }
-    else{
-        return 'scissors'
-    }
-}
+// function getHumanChoice(){
+//     let human = parseInt(prompt('Escolha entre: \n[1] pedra | [2] papel | [3] tesoura: '));
+//     if (human == 1){
+//         return 'rock'
+//     }
+//     else if (human == 2){
+//         return 'paper'
+//     }
+//     else{
+//         return 'scissors'
+//     }
+// }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(human, computer){
+    let divRound = document.querySelector('#round')
+    let pRound = document.createElement('p')
+
     switch(human){
         case 'rock':
             if (computer == 'paper'){
                 computerScore++;
-                console.log(`You lose! ${computer} beats ${human}`)
+                pRound.textContent = `You lose! ${computer} beats ${human}`
             }
-            else if (computer == 'scissors'){
+            else if (computer == 'scissor'){
                 humanScore++;
-                console.log(`You win! ${human} beats ${computer}`)
+                pRound.textContent = `You win! ${human} beats ${computer}`
             }
             else{
-                console.log(`Nobody wins! ${human} and ${computer}`)
+                pRound.textContent = `Nobody wins! ${human} and ${computer}`
             }
             break;
         case 'paper':
-            if (computer == 'scissors'){
+            if (computer == 'scissor'){
                 computerScore++;
-                console.log(`You lose! ${computer} beats ${human}`)
+                pRound.textContent = `You lose! ${computer} beats ${human}`
             }
             else if (computer == 'rock'){
                 humanScore++;
-                console.log(`You win! ${human} beats ${computer}`)
+                pRound.textContent = `You win! ${human} beats ${computer}`
             }
             else{
-                console.log(`Nobody wins! ${human} and ${computer}`)
+                pRound.textContent = `Nobody wins! ${human} and ${computer}`
             }
             break;
-        case 'scissors':
+        case 'scissor':
             if (computer == 'rock'){
                 computerScore++;
-                console.log(`You lose! ${computer} beats ${human}`)
+                pRound.textContent = `You lose! ${computer} beats ${human}`
             }
             else if (computer == 'paper'){
                 humanScore++;
-                console.log(`You win! ${human} beats ${computer}`)
+                pRound.textContent = `You win! ${human} beats ${computer}`
             }
             else{
-                console.log(`Nobody wins! ${human} and ${computer}`)
+                pRound.textContent = `Nobody wins! ${human} and ${computer}`
             }
             break;
     }
+    divRound.appendChild(pRound)
 }
 
 
-function playGame(round){
+function* playGame(round, humanChoice){
+    let divRound = document.querySelector('#round')
+
     for(let i = 1;i<=5;i++){
-        let humanSelection = getHumanChoice();
+        let countRound = document.createElement('p')
+        countRound.textContent = `Round ${i}:`
+
+        divRound.appendChild(countRound);
+
+        let pRound = document.createElement('p')
+
+        let humanSelection = yield humanChoice;
         let computerSelection = getComputerChoice();
+        
         round(humanSelection, computerSelection);
-        console.log(`Score: H ${humanScore} x C ${computerScore}`);
+
+        pRound.textContent = `Score: H ${humanScore} x C ${computerScore}`;
+        divRound.appendChild(pRound)
     }
 
-    if (humanScore > computerScore){
-        return 'Human wins!!!'
+    let winnerRound = document.createElement('h2')
+    if (humanScore > computerScore){   
+        winnerRound.textContent = 'Human wins!!!'
     }
     else if (humanScore == computerScore){
-        return "It's a tie!!"
+        winnerRound.textContent = "It's a tie!!"
     }
     else{
-        return 'Computer wins!!!'
-    }  
+        winnerRound.textContent = 'Computer wins!!!'
+    }
+    divRound.appendChild(winnerRound)
 }
 
-console.log(playGame(playRound));
+
+
+let divGame = document.createElement('div')
+
+//texto do jogo
+let textGame = document.createElement('p')
+textGame.textContent = 'Escolha entre: pedra | papel | tesoura: '
+
+let listButtonGame = document.createElement('ul')
+listButtonGame.style = 'display: flex; flex-Direction: row'
+listButtonGame.style.flex
+
+
+for (let item of ['Rock','Paper','Scissor']){
+    // li
+    let itemButtonGame = document.createElement('li')
+    itemButtonGame.style.margin = '3px';
+    itemButtonGame.style.listStyleType = 'none'
+
+    //button
+    let buttonList = document.createElement('button')
+    buttonList.textContent = item
+    buttonList.id = item.toLowerCase()
+    buttonList.addEventListener('click', ()=>{
+        game.next(item.toLowerCase())
+    })
+
+    //append
+    itemButtonGame.appendChild(buttonList)
+    listButtonGame.appendChild(itemButtonGame)
+}
+divGame.appendChild(textGame)
+divGame.appendChild(listButtonGame)
+let roundGame = document.createElement('div')
+roundGame.id = 'round'
+
+divGame.appendChild(roundGame)
+
+document.body.append(divGame)
+
+
+let game = playGame(playRound);
+game.next()
